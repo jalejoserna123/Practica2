009ffa4f3655a3f9c34adffa6e1e9fef2ae0f159
jalejoserna123 <josea.serna@udea.edu.co>
2018-03-22 10:57:43 -0500

    problema8

--- Problema8/main.cpp
+++ Problema8/main.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
+using namespace std;
+string sepcad(string cad, char *num); //Función que separa los caracteres y los numeros en arreglos diferentes
+int loncad(string cad); //Función que da la longitud de la cadena
 //programa que recibe una cadena de caracteres y separa los números del resto de caracteres,
 //generando una cadena que no tiene números y otra con los números que había en la cadena original.
 //Ejemplo: Si se recibe abc54rst el programa debe imprimir las cadenas: abcrst y 54.
-using namespace std;
+
 //ASCII Numeros[48-57]
 int main()
 {
-    char * cadena_dinamica = NULL;
-    int cantidad;
-    cout << "Ingrese el tamaño de la cadena : " << endl; //obtenemos un valor de cantidad capturado de consola
-    cin >> cantidad;
-    cadena_dinamica = new char[cantidad]; // creamos el arreglo dinámico con el parametro capturado
-    cout << "Ingrese su cadena de caracteres"<< endl;
-    cin >> cadena_dinamica;
-     for(int i = 0; i < cantidad; i++){
-         cout <<cadena_dinamica[i];
-     }
-
-
-
-
-
-
-
-
-
-    delete [] cadena_dinamica;
-    return 0;
+   int longitud;
+   string original, caracteres;
+   cout << " Ingrese la cadena: " << endl;
+   getline(cin, original); //Capturar la cadena
+   longitud=loncad(original); //USo de la funcion para calcular la longitud de la cadena
+   char numeros[longitud]; // cadena que guardara los numeros de la cadena original
+   caracteres = sepcad(original, numeros); //separacion de numeros y caracteres
+   cout << "Original: " << original << endl;
+   cout << "Texto: " << caracteres << ". " << "Numeros: " << numeros <<"."<<endl;
+   return 0;
 }
+string sepcad(string cad, char *num){
+    int contn = 0;
 
+    for(int i=0;cad[i];i++){
+
+        if(cad[i] >= 48 && cad[i] <= 57){
+            *(num+contn) = cad[i];
+            cad[i] = 0;
+         }
+        //else
+        //*(num+contn)=' ';
+        contn += 1;
+}
+    return cad;
+
+}
+int loncad(string cad){
+    int lon;
+    for(int i=0; cad[i]; i++){
+        lon = i+1;
+    }
+    return lon;
+}
